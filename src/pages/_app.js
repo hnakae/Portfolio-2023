28ca224df19a1e13c4ab18667915645cdfa1563f
hnakae <nakae27@gmail.com>
2023-04-18 13:27:44 -0700

    navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
