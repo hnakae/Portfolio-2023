e0884d3e8e6e896c9f45ef54d45b5bebcab77152
hnakae <nakae27@gmail.com>
2023-04-18 14:39:15 -0700

    light color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,16 +15,18 @@ module.exports = {
       },
       colors: {
         dark: "#1b1b1b",
-        light: "#f5f5f5",
+        // light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
+        light: "#f0e7db",
       },
       animation: {
         "spin-slow": "spin 8s linear infinite",
       },
       backgroundImage: {
         circularLight:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
+          // "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
+          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f0e7db 5px,#f0e7db 100px)",
 
         circularDark:
           "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px)",
