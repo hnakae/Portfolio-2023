36b23e1b6c5db636e58c52eebb14276a987eb5dc
hnakae <nakae27@gmail.com>
2023-04-18 19:36:15 -0700

    penguin

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import React, { useEffect, useRef } from "react";
-import nid from "../../public/assets/capture.png";
+// import nid from "../../public/assets/capture.png";
+import penguin from "../../public/assets/penguin.jpg";
 import { useMotionValue, useSpring, useInView } from "framer-motion";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
@@ -72,7 +73,7 @@ const about = () => {
             <div className="col-span-3 relative h-max rounded-3xl rounded-br-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl " />
               <Image
-                src={nid}
+                src={penguin}
                 alt="Hironobu"
                 className="w-full h-auto rounded-2xl"
                 priority
