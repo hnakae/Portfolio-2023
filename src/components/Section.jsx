6850e6a57905544031876066643ae0c217af1da1
hnakae <nakae27@gmail.com>
2023-05-08 00:45:14 -0700

    define next tasks

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -13,17 +13,33 @@ const Section = () => {
       <div className="flex justify-center items-center pt-12">
         <div className="w-[300px] h-[300px] rounded-[10px] bg-brandBlue2 mx-6">
           <div className="flex justify-center items-center p-4">
-            break up index/home page into section components
+            break up index/home page into section components -done :D
           </div>
         </div>
         <div className="w-[300px] h-[300px] rounded-[10px] bg-white mx-6">
           <div className="flex justify-center items-center p-4">
-            Mobile-first Tailwind
+            Mobile-first Tailwind by sections
+            <ul>
+              <li>Hero</li>
+              <li>Projects</li>
+              <li>Skills</li>
+              <li>About</li>
+              <li>Section</li>
+              <li>Contact</li>
+              <li>Footer</li>
+            </ul>
           </div>
         </div>
         <div className="w-[300px] h-[300px] rounded-[10px] bg-brandPurple mx-6">
           <div className="flex justify-center items-center p-4">
             useAnimate with Framer Motion
+            <ul>
+              <li>Hero</li>
+              <li>Projects</li>
+              <li>Skills</li>
+              <li>About</li>
+              <li>Section</li>
+            </ul>
           </div>
         </div>
       </div>
